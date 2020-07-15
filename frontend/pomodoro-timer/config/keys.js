31f6bdedfd1cff6bd1f8acca62c18ed488a0b26e
Chenxing <liuchenxing90@gmail.com>
2020-07-14 19:11:16 -0700

    Category and tasks dynamically rendered based on user email

--- frontend/pomodoro-timer/config/keys.js
+++ frontend/pomodoro-timer/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-	googleClientID: ''
+	googleClientID: '848521743616-tkbpruiqgu7mf5587rcvmcq1pklvcd5k.apps.googleusercontent.com'
 };
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,58 +1,136 @@
 const express = require('express');
 const mongodb = require('mongodb');
-const mongoose = require('mongoose');
 const keys = require('../config/keys');
-
 const router = express.Router();
 
-//Import data schema
-const Category = require('../models/Category');
-
-//Get categories
-router.get('/categories', async (req, res) => {
-  const categories = await loadPostsCollection();
-	console.log(categories);
-  res.send(await categories.find({}).toArray());
+//Post user ID to DB
+router.post('/user', async(req,res) => {
+	// Get user collection from DB
+	const db = await loadDB();
+	const user = db.collection('User');
+	// Check if user exists by email(ID)
+	const searchRes = await user.findOne({_id: req.body.userID});
+	// New user
+	if (searchRes == null) {
+		console.log("Result is null. Add new user");
+		await user.insertOne({
+			// console.log(req.body);
+			_id: req.body.userID
+		});
+	}
+	// Existing user
+	else {
+		console.log("Existing user. Go get category");
+	}
+	// load current user category
+	const category = db.collection('Category');
+	// Get categories from db based on query criteria
+	let temp_res = await category.find({userID: req.body.userID}).toArray();
+	// Parse result before sending back
+	let temp_list = [];
+	for (let element of temp_res) {
+		temp_list.push(element.categName)
+	}
+	// Send back categories under current category
+	res.send(temp_list)
+	// res.send(await category.find({userID: req.body.userID}).toArray());
 });
 
 
-//
-async function loadPostsCollection() {
+//Post category to DB
+router.post('/category', async(req,res) => {
+	// Get category collection from DB
+	const db = await loadDB();
+	const category = db.collection('Category');
+	// Insert category with userID (email)
+	await category.insertOne({
+		userID: req.body.userID,
+		categName: req.body.categName
+	});
+
+	// Get categories from db based on query criteria
+	let temp_res = await category.find({userID: req.body.userID}).toArray();
+	// Parse result before sending back
+	let temp_list = [];
+	for (let element of temp_res) {
+		temp_list.push(element.categName)
+	}
+	// Send back updated categories
+	res.send(temp_list)
+	// res.send(await category.find({userID: req.body.userID}).toArray());
+});
+
+
+
+//Get task from DB
+router.get('/task', async(req,res) => {
+	// Get category collection from DB
+	const db = await loadDB();
+	const task = db.collection('Task');
+	// console.log(req.query);
+	// Get tasks from db based on query criteria
+	let temp_res = await task.find({userID: req.query.userID, categName: req.query.categName}).toArray();
+	// Parse result before sending back
+	let temp_list = [];
+	for (let element of temp_res) {
+		temp_list.push(element.taskName)
+	}
+	// Send back tasks under current category
+	res.send(temp_list)
+	// res.send(await task.find({userID: req.query.userID, categName: req.query.categName}).toArray());
+});
+
+
+//Post task to DB
+router.post('/task', async(req,res) => {
+	// Get category collection from DB
+	const db = await loadDB();
+	const task = db.collection('Task');
+	// Insert task with userID (email) and category name
+	await task.insertOne({
+		userID: req.body.userID,
+		categName: req.body.categName,
+		taskName: req.body.taskName
+	});
+
+	// Get tasks from db based on query criteria
+	let temp_res = await task.find({userID: req.body.userID, categName: req.body.categName}).toArray();
+	// Parse result before sending back
+	let temp_list = [];
+	for (let element of temp_res) {
+		temp_list.push(element.taskName)
+	}
+	// Send back updated tasks
+	res.send(temp_list)
+	// res.send(await task.find({userID: req.body.userID, categName: req.body.categName}).toArray());
+});
+
+
+// Utility function
+async function loadDB() {
   const client = await mongodb.MongoClient.connect(
     'mongodb+srv://Chx:MongoDBChx@pomodorocluster.fvkty.mongodb.net/Timer?retryWrites=true&w=majority',
     {
       useNewUrlParser: true, useUnifiedTopology: true
     }
   );
-
-  return client.db('Timer').collection('Category');
+	return client.db('Timer');
 }
 
-
-// router.get("/category", (req, res) => {
-// 	console.log("category requested");
-//   Category.find({})
-//     .sort({ update_at: -1 })
-//     .then(cats => {
-//       res.json(cats);
-// 			console.log("In find");
-//     })
-//     .catch(err => {
-//       console.log(2);
-//       res.json(err);
-//     });
-// });
-
-
-// Add Post
-// router.post('/', async (req, res) => {
-//   const posts = await loadPostsCollection();
-//   await posts.insertOne({
-//     text: req.body.text,
-//     createdAt: new Date()
-//   });
-//   res.status(201).send();
-// });
-//
-
 module.exports = router;
+
+//Import data schema
+// const mongoose = require('mongoose');
+// const category = require('../models/category');
+// const user = require('../models/user');
+
+// try {
+// 	const user = await loadUserCollection();
+// 	console.log(user);
+// 	user.insertOne({
+// 		_id: req.body.userID
+// 	});
+// 	res.status(201).send();
+// } catch(e) {
+// 	console.log(e);
+// };
