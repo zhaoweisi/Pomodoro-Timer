e0e667b39b29037712066c5558271e019738bba1
Chenxing <liuchenxing90@gmail.com>
2020-07-22 15:20:12 -0700

    Timer & reward function added. Task selection updated.

--- frontend/pomodoro-timer/src/components/LoginButton.vue
+++ frontend/pomodoro-timer/src/components/LoginButton.vue
@@ -2,11 +2,11 @@
 
   <div>
     <button v-google-signin-button="clientId" class="google-signin-button" v-if="!isHidden">
-      <img alt="Google sign-in" src="../assets/icons/btn_google_signin_dark_normal_web.png" />
-      </button>
+            <img alt="Google sign-in" src="../assets/icons/btn_google_signin_dark_normal_web.png" />
+            </button>
     <br>
     <button v-google-signout-button="clientId" class="google-signout-button" v-if="isSignOutButtonHidden"> Sign out
-      </button>
+            </button>
     <br>
     <br>
     <br>
@@ -17,25 +17,58 @@
     <br>
     <div v-if="isHidden"> Your Name: {{this.userFullName}}</div>
     <div v-if="isHidden"> Your Email: {{this.userEmail}}</div>
+		<div v-if="isHidden"> Your Reward: {{this.userReward}}</div>
 
-    <br>
+    <br><br>
+
+    <!-- Timer -->
+    <div v-if="isHidden">
+
+      <div v-if="!isTaskChosen"> Start a potato session by clicking a task name </div>
+      <div v-else>
+        <p> To switch between tasks, just click task name </p>
+        <p style="font-weight: bold"> Selected task: {{selectedTask}}  <button @click="onTimesUp()" >Mark as completed</button> </p>
+        <p> Remaining time total: {{taskRemDisplay}} </p>
+      </div>
+      <div v-if="!timerStarted && isTaskChosen">
+        Set {{isTask}} session span (mins)
+        <input v-model="timerInput" placeholder="edit me">
+        <button @click="startTimer()" v-bind:disabled="timerInput < 1">Start</button>
+        <br>
+      </div>
+
+      <div v-if="timerStarted">
+        <div>{{timeDisplay}}</div>
+        <button @click="pauseTimer()"> Pause/Resume </button>
+        <!-- <button @click="skip()"> Skip </button> -->
+      </div>
+      <div v-if="isTaskChosen">
+        <button @click="skip()"> Skip </button>
+      </div>
+    </div>
+
+    <!-- Category and task list -->
+    <br> <br>
     <div v-if="isHidden">
       <!-- Load current user's categories -->
       <h2> Your categories: </h2>
-			<br>
+      <p> Click category name to add or show tasks</p>
+      <br>
       <ul v-for="(catgItem, index) in categoryList" :key="catgItem">
         <li @click="showTask(catgItem, index)" style="font-weight: bold">{{catgItem}}</li>
 
         <!-- Show clicked category's task list -->
         <div v-if="index==clickedCatgIndex">
           <ul v-for="taskItem in taskList" :key="taskItem">
-            <li> {{taskItem}} </li>
+            <li @click="selectTask(taskItem, catgItem)"> {{taskItem}} </li>
           </ul>
           <!-- Send Ajax request to add tasks -->
           <button v-if="showAddTaskButton" @click="showTaskInput()">Add Task</button>
-          <button v-else @click="addTask(catgItem)" v-bind:disabled="taskName.length < 1">Submit</button>
-          <input v-if="showTaskInputForm" v-model="taskName" placeholder="edit me">
+          <input v-if="showTaskInputForm" v-model="taskName" placeholder="task name">
+          <input v-if="showTaskInputForm" v-model="taskSpan" placeholder="task span">
           <p v-if="showTaskInputForm">New task name is: {{ taskName }}</p>
+          <p v-if="showTaskInputForm">New task span is: {{ taskSpan }} minutes </p>
+          <button v-if="!showAddTaskButton" @click="addTask(catgItem)" v-bind:disabled="taskName.length < 1 || taskSpan < 1">Submit</button>
         </div>
 
       </ul>
@@ -72,12 +105,30 @@
       userFullName: '',
       userEmail: '',
       imgURL: '',
+			userReward: null,
+      // Ajax
       userID: '',
-      catgName: '',
-      taskName: '',
+      catgName: '', // for addCategory()
+      taskName: '', // for addTask()
+      taskSpan: null, // User input for addTask (mins)
+      taskRemTotal: null, // Data pulled from DB (converted to secs)
+      taskRemDisplay: null, // for display
       clickedCatgIndex: 0,
       categoryList: [],
-      taskList: []
+      taskList: [],
+      // Timer
+      timerInput: 0, // timer session span input (mins)
+      timerStarted: false,
+      isTaskChosen: false,
+      selectedTask: '', // pull selected task time data from DB
+      selectedCatg: '', // pull selected task time data from DB
+      isTask: 'task',
+      timeDisplay: '', // timer session display
+      timeTotal: 5, // timer session span, converted to seconds
+      timePassed: 0, // +1 every update, sec
+      timeRemaining: 0, // -1 every update, sec, init to timeTotal
+      interval: null,
+      isPaused: false
     }),
 
     methods: {
@@ -90,8 +141,6 @@
         console.log('You have signed in')
         this.isHidden = true
         this.isSignOutButtonHidden = true
-        // Upon user login, load categories
-        this.getCategory()
         // Upon user login, add user to db and load categories
         this.addUserID()
       },
@@ -100,16 +149,18 @@
         console.log(error)
       },
 
-      //Upon user login, add user to db and load user's categories
+      //Upon user login, add user to db and load user's categories and reward
       addUserID() {
         // promise production
         axios
           .post('http://localhost:5000/api/user', { userID: this.userEmail })
           // promise comsumption
           .then(res => {
-						console.log("In addUserID load categories")
+            console.log('In addUserID load categories and reward')
             console.log(res.data)
-            this.categoryList = res.data
+						// Parse response and show on page
+						this.userReward = res.data[0]
+            this.categoryList = res.data.slice(1,)
           })
       }, //addUserID() barcket
 
@@ -125,14 +176,14 @@
         this.showTaskInputForm = true
       }, //showInput() barcket
 
-			//
+      //
       addCategory() {
         // promise production
         axios
           .post('http://localhost:5000/api/category', { userID: this.userEmail, categName: this.catgName })
           // promise comsumption
           .then(res => {
-						console.log("In add Category")
+            console.log('In add Category')
             console.log(res.data)
             // Show updated categories
             this.categoryList = res.data
@@ -158,16 +209,15 @@
         }).then(res => {
           console.log('In showtask')
           console.log(res.data)
-					this.taskList = res.data
+          this.taskList = res.data
         })
       },
+
       OnGoogleAuthSignOut() {
-        console.log("You have signed out")
+        console.log('You have signed out')
         this.isHidden = false
         this.isSignOutButtonHidden = false
-      }
-
-    }
+      },
 
       //
       addTask(catgItem) {
@@ -176,11 +226,13 @@
           .post('http://localhost:5000/api/task', {
             userID: this.userEmail,
             categName: catgItem,
-            taskName: this.taskName
+            taskName: this.taskName,
+            taskSpan: parseInt(this.taskSpan * 60, 10),
+						status: "ongoing"
           })
           // promise comsumption
           .then(res => {
-						console.log("In add task")
+            console.log('In add task')
             console.log(res.data)
             // Show updated tasks
             this.taskList = res.data
@@ -190,16 +242,222 @@
             (this.showTaskInputForm = false)
             // this.taskName = ""     //   clear input doesn't work ????????????????????
           )
-      } //addTask() bracket
+      }, //addTask() bracket
+
+      //
+      selectTask(taskItem, catgItem) {
+        if (!this.timerStarted) {
+          // Show current selected task remaining time total
+          axios({
+            method: 'get',
+            url: 'http://localhost:5000/api/taskSpan',
+            params: {
+              userID: this.userEmail,
+              categName: catgItem,
+              taskName: taskItem
+            }
+          }).then(res => {
+						console.log("In select task. Print task candidate.")
+            // console.log(res.data)
+						// Check task status. If ongoing, start timer. If depleted or finished, alert
+						if (res.data.status == "ongoing") {
+							// Show timer
+							this.isTaskChosen = true
+							// Prep data for timer
+							this.taskRemTotal = res.data.taskSpan
+							this.taskRemDisplay = this.timeToString(this.taskRemTotal)
+							// Show task name above timer
+							this.selectedTask = taskItem
+							this.selectedCatg = catgItem
+							// console.log(taskItem)
+						}
+						// Task already ended. Hide timer
+						else {
+							this.isTaskChosen = false
+							alert("Task already ended! Choose another one.")
+						}
+          })
+        }
+      },
+
+      //
+      onTimesUp() {
+        clearInterval(this.interval)
+        this.timerStarted = false
+				console.log('In time is up, show time passed.')
+				console.log(this.timePassed)
+				axios
+					.post('http://localhost:5000/api/taskSpan', {
+						userID: this.userEmail,
+						categName: this.selectedCatg,
+						taskName: this.selectedTask,
+						subtraction: this.timePassed
+					})
+					// promise comsumption
+					.then(res => {
+						console.log('In time is up promise then')
+						console.log(res.data)
+					})
+					// Ask user if task is finished or not
+					.then(() => {
+						// Finished, increment reward
+						if (confirm("Mark task as finished?") == true) {
+							this.userReward = this.userReward+1;
+							// Add visualization...
+							// Update reward in database
+							axios
+								.post('http://localhost:5000/api/reward', {
+									userID: this.userEmail,
+								})
+								// promise comsumption
+								.then(res => {
+									console.log('In updated reward response')
+									console.log(res.data)
+								})
+						}
+						else {
+							alert("Tips: Set more realistic task span next time!")
+						}
+					})
+					// Hide timer and go back to choose task
+					.then(() => {
+						// Change task status in db
+						axios
+							.post('http://localhost:5000/api/status', {
+								userID: this.userEmail,
+								categName: this.selectedCatg,
+								taskName: this.selectedTask,
+								taskStatus: "ended"
+							})
+						// Hide timer
+						this.isTaskChosen = false
+					})
+      },
+
+      //
+      reset() {
+        clearInterval(this.interval)
+        this.timePassed = 0
+        this.timeRemaining = this.timeTotal
+        this.isPaused = false
+        // this.update()
+      },
+
+      //
+      update() {
+        this.timeRemaining = this.timeTotal - this.timePassed
+        this.timeDisplay = this.timeToString(this.timeRemaining)
+        // console.log("taskRemTotal")
+        // console.log(this.taskRemTotal)
+				if (this.isTask == "task") {
+					this.taskRemDisplay = this.timeToString(this.taskRemTotal - this.timePassed)
+				}
+      },
+
+      //
+      startTimer() {
+        // Request to get current task's time data
+        axios({
+          method: 'get',
+          url: 'http://localhost:5000/api/taskSpan',
+          params: {
+            userID: this.userEmail,
+            categName: this.selectedCatg,
+            taskName: this.selectedTask
+          }
+        }).then(res => {
+          // console.log(res.data)
+          this.taskRemTotal = res.data.taskSpan
+          this.timeTotal = this.timerInput * 60
+					if (this.isTask == "task" && this.taskRemTotal <= this.timeTotal) {
+						this.timeTotal = this.taskRemTotal
+					}
+          this.timerStarted = true
+          this.reset()
+          this.interval = setInterval(() => {
+            if (this.isPaused) return
+            this.timePassed += 1
+            this.update()
+						//
+            if (this.timeRemaining === 0) {
+              this.onTimesUp()
+            }
+          }, 1000)
+        })
+      },
+
+      // Switch between task and rest
+      skip() {
+        if (this.timerStarted && this.isTask == "task") {
+          console.log('In skip')
+          console.log(this.timePassed)
+          axios
+            .post('http://localhost:5000/api/taskSpan', {
+              userID: this.userEmail,
+              categName: this.selectedCatg,
+              taskName: this.selectedTask,
+              subtraction: this.timePassed
+            })
+            // promise comsumption
+            .then(res => {
+              console.log('In skip promise then')
+              console.log(res.data)
+            })
+        }
+        this.reset() // clear interval
+        this.timerStarted = false
+        // this.isRest = !this.isRest
+        if (this.isTask == 'rest') {
+          this.isTask = 'task'
+        } else {
+          this.isTask = 'rest'
+        }
+      },
+
+      //
+      stopTimer() {
+        clearInterval(this.interval)
+      },
+
+      //
+      pauseTimer() {
+        this.isPaused = !this.isPaused
+        console.log('Rem')
+        console.log(this.timeRemaining)
+        console.log('Used')
+        console.log(this.timePassed)
+      },
+
+      //
+      pad(time) {
+        if (time < 10) return `0${time}`
+        return `${time}`
+      },
+
+      //
+      timeToString(time) {
+        let minutes = Math.floor(time / 60)
+        let seconds = time % 60
+
+        seconds = this.pad(seconds)
+        minutes = this.pad(minutes)
+
+        return `${minutes}:${seconds}`
+      }
+
     } // methods bracket
-  }
+  } // export bracket
 
 </script>
 
+
+
+
 <style scoped>
+
   .google-signin-button {
     display: inline-block;
-    background-color:transparent;
+    background-color: transparent;
     vertical-align: middle;
     border-style: unset;
   }
@@ -217,7 +475,8 @@
     font-size: 14px;
   }
 
-img {
-  border-radius: 0px;
-}
+  img {
+    border-radius: 0px;
+  }
+
 </style>
--- server/routes/api.js
+++ server/routes/api.js
@@ -10,28 +10,35 @@ router.post('/user', async(req,res) => {
 	const user = db.collection('User');
 	// Check if user exists by email(ID)
 	const searchRes = await user.findOne({_id: req.body.userID});
+	let temp_list = [];
 	// New user
 	if (searchRes == null) {
 		console.log("Result is null. Add new user");
 		await user.insertOne({
 			// console.log(req.body);
-			_id: req.body.userID
+			_id: req.body.userID,
+			reward: 0
 		});
+		temp_list.push(0);
 	}
 	// Existing user
 	else {
-		console.log("Existing user. Go get category");
+		console.log("Existing user. Go get category and reward");
+		// Append reward to result list
+		temp_list.push(searchRes.reward);
+		// console.log(temp_list);
 	}
+	// console.log(searchRes);
 	// load current user category
 	const category = db.collection('Category');
 	// Get categories from db based on query criteria
 	let temp_res = await category.find({userID: req.body.userID}).toArray();
 	// Parse result before sending back
-	let temp_list = [];
 	for (let element of temp_res) {
+		// Append category names to result list
 		temp_list.push(element.categName)
 	}
-	// Send back categories under current category
+	// Send back categories under current category and user reward
 	res.send(temp_list)
 	// res.send(await category.find({userID: req.body.userID}).toArray());
 });
@@ -90,7 +97,8 @@ router.post('/task', async(req,res) => {
 	await task.insertOne({
 		userID: req.body.userID,
 		categName: req.body.categName,
-		taskName: req.body.taskName
+		taskName: req.body.taskName,
+		taskSpan: req.body.taskSpan
 	});
 
 	// Get tasks from db based on query criteria
@@ -106,6 +114,73 @@ router.post('/task', async(req,res) => {
 });
 
 
+//Get task span from DB
+router.get('/taskSpan', async(req,res) => {
+	// Get category collection from DB
+	const db = await loadDB();
+	const task = db.collection('Task');
+	console.log("In taskSpan get");
+	console.log(req.query);
+	res.send(await task.findOne({userID: req.query.userID, categName: req.query.categName, taskName: req.query.taskName}))
+});
+
+
+//Update task span from DB
+router.post('/taskSpan', async(req,res) => {
+	// Get category collection from DB
+	const db = await loadDB();
+	const task = db.collection('Task');
+	console.log("In taskSpan post");
+	console.log(req.body);
+	// Subtract timePassed from taskSpan and update
+	let result = await task.updateOne(
+		{userID: req.body.userID, categName: req.body.categName, taskName: req.body.taskName},
+		{
+			$inc: {taskSpan: (-1)*req.body.subtraction}
+		}
+	);
+	res.sendStatus(200)
+});
+
+
+//Update user reward in DB
+router.post('/reward', async(req,res) => {
+	// Get category collection from DB
+	const db = await loadDB();
+	const user = db.collection('User');
+	console.log("In reward post");
+	console.log(req.body);
+	// Update reward
+	let result = await user.findOneAndUpdate(
+		{_id: req.body.userID},
+		{
+			// $inc: {reward: (+1)*req.body.userReward}
+			$inc: {reward: 1}
+		}
+	);
+	// let result = await user.findOne({_id: req.body.userID})
+	// console.log(result);
+	res.sendStatus(200)
+});
+
+
+//Update task status in DB
+router.post('/status', async(req,res) => {
+	// Get task collection from DB
+	const db = await loadDB();
+	const task = db.collection('Task');
+	console.log("In status post");
+	console.log(req.body);
+	let result = await task.findOneAndUpdate(
+		{userID: req.body.userID, categName: req.body.categName, taskName: req.body.taskName},
+		{
+			$set: {status: req.body.taskStatus}
+		}
+	);
+	res.sendStatus(200)
+});
+
+
 // Utility function
 async function loadDB() {
   const client = await mongodb.MongoClient.connect(
@@ -118,11 +193,6 @@ async function loadDB() {
 }
 
 module.exports = router;
-
-//Import data schema
-// const mongoose = require('mongoose');
-// const category = require('../models/category');
-// const user = require('../models/user');
 
 // try {
 // 	const user = await loadUserCollection();
