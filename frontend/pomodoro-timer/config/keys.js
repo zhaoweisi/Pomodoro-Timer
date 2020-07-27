b62d866fb4ecb32337b94af30ab18af7c7e62b81
Yuan Li <liyu4@oregonstate.edu>
2020-07-27 11:15:49 -0700

    put frontend intoserver

--- frontend/pomodoro-timer/config/keys.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = { 
-	googleClientID: '404182910107-s7pl9tqlp3as356135mj33j1rp7tlhf8.apps.googleusercontent.com',
-	googleClientSecret: 'aeH_KOjSLarsvYA_q7E9aYLq',
-	mongoURI: 'mongodb+srv://gak27ly:XD5DzVUjsJzcEqCT@mycampagin.m7rag.mongodb.net/Mycampagin?retryWrites=true&w=majority',
-	cookieKey: 'ijioehronsjdfiodshfiofiojsdoifjoiewnfjs'
-};
