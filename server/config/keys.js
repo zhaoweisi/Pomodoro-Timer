6c561f271948bab610688312b73c3232bd181783
Chenxing <liuchenxing90@gmail.com>
2020-08-05 15:11:35 -0700

    push without server file

--- server/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	googleClientID: '848521743616-tkbpruiqgu7mf5587rcvmcq1pklvcd5k.apps.googleusercontent.com',
-	googleClientSecret: 'MMWbkud8oMHGo20vpKXmmP91',
-	mongoURI:'mongodb+srv://Chx:MongoDBChx@pomodorocluster.fvkty.mongodb.net/sample_airbnb?retryWrites=true&w=majority',
-	cookieKey:'Potato'
-};
